9cc8a06f983fde2b2e9bfe1ddeac6908fecef019
⚡ Sigui <siguici@proton.me>
2025-03-08 06:01:02 +0100

    🩹 Fix public directory

--- src/resources/app.tsx
+++ src/resources/app.tsx
@@ -1,7 +1,7 @@
 import { component$, useSignal } from "@builder.io/qwik";
 
-import viteLogo from "/vite.svg";
 import qwikLogo from "./qwik.svg";
+import viteLogo from "./vite.svg";
 import "./app.css";
 
 export const App = component$(() => {
